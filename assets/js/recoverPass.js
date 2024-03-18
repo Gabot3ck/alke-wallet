1f366ef06f1aa69073c9a86a55a7eb8c1082f069
Gabot3ck <jesus.castilloc@outlook.com>
2024-03-18 18:00:18 -0300

    se crea la funcion para recuperar contraseña

---
+++ assets/js/recoverPass.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    // PASSWORD RECOVERY //
+    $("#btnToRecoverPass").click( function(e) {
+
+      (async () => {
+  
+        const { value: email } = await Swal.fire({
+          title: 'Ingrese su dirección de email:',
+          input: 'email',
+          inputPlaceholder: 'ejemplo@email.com',
+          inputValidator: (value) => {
+            if (!value || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+              return "Dirección de email no válida";
+            }
+          }
+        });
+        
+        if (email) {
+          Swal.fire(`Se envío un email a: ${email}`)
+        }
+        
+      })();
+    })
+    // END PASSWORD RECOVERY //
+
+})
+
